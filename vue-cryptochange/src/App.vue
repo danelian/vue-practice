7e7dbb7fe7e4f20ac902142bd26f104f3ac8cbcc
danelian <141317269+danelian@users.noreply.github.com>
2024-01-03 23:03:29 +0400

    crypto change - verstka

---
+++ vue-cryptochange/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import Input from './components/Input.vue'
+import Selector from './components/Selector.vue'
+
+export default {
+  components: {
+    Input,
+    Selector
+  }
+}
+</script>
+
+<template>
+  <h1>Crypto</h1>
+  <Input />
+  <div className="selectors">
+    <Selector />
+    <Selector />
+  </div>
+</template>
+
+<style scoped>
+.selectors {
+  display: flex;
+  justify-content: space-between;
+  width: 700px;
+  margin: 0 auto;
+}
+</style>
---
+++ vue-cryptochange/src/components/Input.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <input type="number">
+</template>
+
+<style scoped>
+input {
+  outline: none;
+  width: 500px;
+  position: relative;
+  top: -50px;
+  font-size: 1.25em;
+  border-radius: 5px;
+  border: 4px solid #1a032d;
+  padding: 15px 20px;
+  color: #333;
+  background: #fafafa;
+}
+</style>
