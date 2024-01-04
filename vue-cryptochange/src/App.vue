a6ec2589fc488defcd37f6798518c926e0658eae
danelian <141317269+danelian@users.noreply.github.com>
2024-01-04 14:31:35 +0400

    convert
    crypto-convert api

--- vue-cryptochange/src/App.vue
+++ vue-cryptochange/src/App.vue
@@ -1,21 +1,75 @@
 <script>
+import CryptoConvert from 'crypto-convert'
 import Input from './components/Input.vue'
 import Selector from './components/Selector.vue'
+
+const convert = new CryptoConvert()
 
 export default {
   components: {
     Input,
     Selector
+  },
+  data() {
+    return {
+      amount: 0,
+      cryptoFirst: '',
+      cryptoSecond: '',
+      error: '',
+      result: 0
+    }
+  },
+  methods: {
+    changeAmount(val) {
+      this.amount = val
+    },
+    setCryptoFirst(val) {
+      this.cryptoFirst = val
+    },
+    setCryptoSecond(val) {
+      this.cryptoSecond = val
+    },
+    async convert() {
+      if (this.amount <= 0) {
+        this.error = 'Введите число больше за ноль'
+        return
+      } else if (this.cryptoFirst == '' || this.cryptoSecond == '') {
+        this.error = 'Выберите валюту'
+        return
+      } else if (this.cryptoFirst == this.cryptoSecond) {
+        this.error = 'Выберите другую валюту'
+        return
+      }
+
+      this.error = ''
+
+      await convert.ready()
+
+      if (this.cryptoFirst == 'BTC' && this.cryptoSecond == 'ETH')
+        this.result = convert.BTC.ETH(this.amount)
+      else if (this.cryptoFirst == 'BTC' && this.cryptoSecond == 'USDT')
+        this.result = convert.BTC.USDT(this.amount)
+      else if (this.cryptoFirst == 'ETH' && this.cryptoSecond == 'BTC')
+        this.result = convert.ETH.BTC(this.amount)
+      else if (this.cryptoFirst == 'ETH' && this.cryptoSecond == 'USDT')
+        this.result = convert.ETH.USDT(this.amount)
+      else if (this.cryptoFirst == 'USDT' && this.cryptoSecond == 'BTC')
+        this.result = convert.USDT.BTC(this.amount)
+      else if (this.cryptoFirst == 'USDT' && this.cryptoSecond == 'ETH')
+        this.result = convert.USDT.ETH(this.amount)
+    }
   }
 }
 </script>
 
 <template>
   <h1>Crypto</h1>
-  <Input />
+  <Input :changeAmount="changeAmount" :convert="convert" />
+  <p v-if="error != ''">{{ error }}</p>
+  <p v-if="result != 0">{{ result }}</p>
   <div className="selectors">
-    <Selector />
-    <Selector />
+    <Selector :setCrypto="setCryptoFirst" />
+    <Selector :setCrypto="setCryptoSecond" />
   </div>
 </template>
 
@@ -23,7 +77,11 @@ export default {
 .selectors {
   display: flex;
   justify-content: space-between;
-  width: 700px;
+  width: 600px;
   margin: 0 auto;
+}
+p {
+  font-size: 20px;
+  color: #fff;
 }
 </style>
--- vue-cryptochange/src/components/Input.vue
+++ vue-cryptochange/src/components/Input.vue
@@ -1,17 +1,27 @@
 <script>
 export default {
-
+  props: {
+    changeAmount: {
+      type: Function,
+      required: true
+    },
+    convert: {
+      type: Function,
+      required: true
+    }
+  }
 }
 </script>
 
 <template>
-  <input type="number">
+  <input min="0" type="number" @input="changeAmount($event.target.value)">
+  <button @click="convert()">Конвертировать</button>
 </template>
 
 <style scoped>
 input {
   outline: none;
-  width: 500px;
+  width: 560px;
   position: relative;
   top: -50px;
   font-size: 1.25em;
@@ -20,5 +30,18 @@ input {
   padding: 15px 20px;
   color: #333;
   background: #fafafa;
+}
+button {
+  position: relative;
+  top: -20px;
+  display: flex;
+  margin: auto;
+  padding: 15px 20px;
+  color: #fff;
+  text-transform: uppercase;
+  cursor: pointer;
+  background: #1a032d;
+  border: 0;
+  border-radius: 3px;
 }
 </style>
